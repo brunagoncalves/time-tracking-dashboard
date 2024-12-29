5c4037ad0f197837a4a4d58370e86e90575ba001
Bruna Goncalves <brunagoncalves.sjb@gmail.com>
2024-12-29 15:29:34 -0300

    Deploy Settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  // base: "/time-tracking-dashboard",
+  base: "/time-tracking-dashboard",
   site: "https://brunagoncalves.github.io",
   integrations: [tailwind(), sitemap()],
 });
